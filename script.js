35d7d337bdf2479ff0b24b997db0e7df6890dc6b
nomorealex <114149640+nomorealex@users.noreply.github.com>
2024-03-20 19:19:01 +0000

    fix(SW-2): adapt media queries to the diferent environments (#2)

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 window.addEventListener("scroll", setScrollVar)
 window.addEventListener("resize", setScrollVar)
 
+function getElementById(ids) {
+  var elements = [];
+  let count = 0
+  ids.forEach(element => {
+    elements[count++] = document.getElementById(element);  
+  });
+  return elements;
+}
+
+function setComponentsVariables(htmlElement) {
+  var ids = ["topicwd", "topicvcs", "topicsd", "topicrd", "topicba", "topictf", "topicwm", "topic3m"];
+  var elements = getElementById(ids);
+  let count = 0;
+  elements.forEach(element => {
+    let rect = element.getBoundingClientRect();
+    let absoluteY = rect.top;
+    htmlElement.style.setProperty(
+      `--${ids[count++]}`,
+      absoluteY
+    )
+  })
+  console.log(elements)
+}
+
 function setScrollVar() {
   const htmlElement = document.documentElement
   const percentOfScreenHeightScrolled =
     htmlElement.scrollTop / htmlElement.clientHeight
   
-    console.log("scrolled:"+percentOfScreenHeightScrolled*100)
+  console.log("scrolled:"+percentOfScreenHeightScrolled*100)
   console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
   htmlElement.style.setProperty(
     "--scrollTotalHeight",
@@ -16,6 +40,8 @@ function setScrollVar() {
     "--scroll",
     Math.min(percentOfScreenHeightScrolled * 100, 100)
   )
+
+ setComponentsVariables(htmlElement);
 }
 
 setScrollVar()
